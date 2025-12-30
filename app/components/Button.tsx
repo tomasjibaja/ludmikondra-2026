7d1fb07b445e29627bcbc6e66b707e90483b9f75
Tomas Jibaja <tomasjibaja@gmail.com>
2025-12-30 03:02:41 -0300

    probando arreglar el problema de tipado en el componente Button

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -1,15 +1,25 @@
 'use client';
+import React, { ReactNode } from "react";
 
-export default function Button ({children, variant}) {
+interface ComponentWithChildrenProps {
+  // Option 1: Use ReactNode
+  children: ReactNode; 
+  // Option 2: Use React.ReactNode
+  // children: React.ReactNode; 
+  variant: string;
+}
+
+const Button: React.FC<ComponentWithChildrenProps> = ({ children, variant }) => {
 
   const variants = {
-    lavanda: 'bg-lavanda text-creamy hover:(text-lavanda bg-creamy)',
-    lima: 'bg-lima text-dark-green',
-    dark: 'bg-leaf-green text-creamy hover:text-leaf-green hover:bg-creamy'
+    'lavanda': 'bg-lavanda text-creamy hover:(text-lavanda bg-creamy)',
+    'lima': 'bg-lima text-dark-green',
+    'dark': 'bg-leaf-green text-creamy hover:text-leaf-green hover:bg-creamy'
   }
 
   return (
-    <button 
+    // rome-ignore lint/a11y/useButtonType: <explanation>
+<button 
       onClick={() => alert('Hiciste click')}
       className={`px-4 py-2 rounded-4xl text-xl font-medium duration-200 hover:cursor-pointer ${variants[variant]}`}
     >
@@ -17,3 +27,5 @@ export default function Button ({children, variant}) {
     </button>
   )
 }
+
+export default Button
