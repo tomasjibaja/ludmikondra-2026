0b4f82449319d6cebd985ae5fd71e286ec745b13
Tomas Jibaja <tomasjibaja@gmail.com>
2026-01-08 11:37:21 -0300

    probando img rota

---
+++ app/loading.tsx
@@ -0,0 +1,9 @@
+export default function Loading() {
+  // You can add any UI inside, including a Skeleton.
+  return (
+    <div className="flex justify-center items-center h-screen text-leaf-green bg-lima">
+      <h1>Cargando...</h1>
+      {/* Optionally add a spinner or skeleton component */}
+    </div>
+  );
+}
