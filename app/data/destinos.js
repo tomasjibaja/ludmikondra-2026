6461bb1933b06a2aa491ee19b0a2380e86067657
Tomas Jibaja <tomasjibaja@gmail.com>
2026-01-08 11:31:46 -0300

    probando cosas para la imagen rota de meditaciones

--- app/data/destinos.js
+++ app/data/destinos.js
@@ -11,7 +11,7 @@ const destinos = [
   { src: '/destinos/aramumuru.jpg', title: 'PORTAL DE ARAMU-MURU' },
   { src: '/destinos/cosmologia.jpeg', title: 'COSMOLOGÍA ANDINA' },
   { src: '/destinos/ceremonias.jpeg', title: 'CEREMONIAS Y OFRENDAS' },
-  { src: '/destinos/meditacion.jpg', title: 'MEDITACIÓN Y YOGA' },
+  { src: '/destinos/medi.jpg', title: 'MEDITACIÓN Y YOGA' },
   { src: '/destinos/portal.jpg', title: 'TEMPLOS INICIÁTICOS' },
   { src: '/destinos/naturaleza.jpeg', title: 'CONEXIÓN CON LA NATURALEZA' }
 ]
