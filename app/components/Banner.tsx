d35e10e7dbc0131df8a30a728b97c9299790cf76
Tomas Jibaja <tomasjibaja@gmail.com>
2026-01-08 11:28:49 -0300

    arreglando img rotas

--- app/components/Banner.tsx
+++ app/components/Banner.tsx
@@ -6,7 +6,7 @@ interface ChildProps {
 
 const Banner = ({ src }: ChildProps) => {
   return (
-    <div className="md:h-120 h-70 max-w-screen overflow-hidden">
+    <div className="md:h-120 h-70 max-w-screen overflow-hidden duration-1000 hover:brightness-110">
         <img className="lg:w-full object-cover" src={`${src}`} alt="banner img"/>
     </div>
   )
