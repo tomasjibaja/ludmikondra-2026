c85e6cf927ca3d1c87815013a36710a15a22e4e0
Tomas Jibaja <tomasjibaja@gmail.com>
2026-03-03 20:35:03 -0300

    cambios a la seccion precios

--- app/components/MachuPichuPrecio.tsx
+++ app/components/MachuPichuPrecio.tsx
@@ -9,25 +9,25 @@ import LinkButton from './LinkButton';
 
 const MachuPichuPrecio = () => {
   return (
-    <div className='flex flex-col gap-4 items-center justify-center bg-creamy text-dark-green pt-10 pb-20 px-10'>
+    <div className='flex flex-col lg:gap-4 gap-7 items-center justify-center bg-creamy text-dark-green pt-10 pb-20 px-10'>
       <h2 className="font-bold xl:text-4xl lg:text-3xl text-2xl max-w-4/5 text-center md:mt-10">
-        PRECIO DEL VIAJE
+        INVERTÍ EN VOS
       </h2>
       <div className='relative flex flex-col items-center justify-center gap-2 md:my-10 lg:py-10 py-7 lg:px-20 px-10 bg-leaf-green rounded-xl text-lima shadow-xl'>
-        <BiSolidBadgeDollar className='absolute top-5 right-5 animate-pulse lg:text-7xl text-3xl' />
-        <span className='lg:text-3xl'>VALOR LANZAMIENTO</span>
-        <span className='lg:text-6xl text-2xl font-semibold flex items-center gap-2'>$ <b className='font-bold lg:text-7xl text-5xl'>2500</b> USD.-</span>
-        <span className='text-xl'>hasta el <b>31/01/26</b></span>
+        <BiSolidBadgeDollar className='absolute top-5 right-5 animate-pulse lg:text-5xl text-3xl' />
+        <span className='lg:text-2xl text-xl text-center'>DESCUENTO EXCLUSIVO DE</span>
+        <span className='lg:text-6xl text-2xl font-semibold flex items-center gap-2'>$ <b className='font-bold lg:text-7xl text-5xl'>150</b> USD.-</span>
+        <span className='lg:text-lg text-md'>agendando tu llamada</span>
       </div>
-      <div className='flex flex-col items-center justify-center gap-4'>
-        <span className='lg:text-2xl'>VALOR REGULAR</span>
-        <h2 className='lg:text-5xl text-3xl font-semibold'>$ <b className='font-bold lg:text-6xl text-4xl'>2800</b> USD.-</h2>
-        <span className='lg:text-xl'>Por persona - Base doble</span>
-        <span className='lg:text-lg text-center font-medium text-dark-green'>
-          <b>Señá tu cupo con 350 USD</b> y pagá en cuotas
-        </span>
+      <div className='flex flex-col items-center justify-center gap-4 rounded-xl border-1 border-leaf-green-50 lg:px-10 px-7 py-4 shadow-md'>
+        <span className='lg:text-2xl text-center'>RESERVA HOY<br/>solo con</span>
+        <h2 className='lg:text-5xl text-3xl font-semibold'>$ <b className='font-bold lg:text-6xl text-4xl'>350</b> USD.-</h2>
+        <span className='lg:text-xl'>de seña y pagá en cuotas</span>
       </div>
-      <span className='lg:text-xl font-semibold text-center'>CIERRE DE INSCRIPCIÓN 30 DE MARZO 2026</span>
+      <span className='lg:text-xl text-center font-normal text-dark-green flex flex-col'>
+        <b>Valor regular: $ 2800 USD.-</b>
+        <span className="lg:text-lg font-medium text-center">Por persona - Base doble</span>
+      </span>
       <LinkButton text='RESERVAR' variant='dark' link='#reservar'/>
       <div className='flex md:flex-row flex-col md:items-start items-center justify-evenly text-center pt-10 w-4/5 md:gap-0 gap-10'>
         <div className='lg:w-4/9 md:w-3/7 w-full flex flex-col items-start justify-center gap-3'>
